1717be50bfe5345c51f1fe9db9757d584c6c763c
maslo0396 <ilya97750@gmail.cm>
2025-03-17 20:02:46 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");				
+const { type } = require('os');
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development';
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: 'index.[contenthash].js',
+        assetModuleFilename: 'assets/[name][ext]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'index.[contenthash].css',
+        })
+
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+              },
+          
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,                     
+                use: [
+                    devMode ? 'style-loader' :  MiniCssExtractPlugin.loader, 
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [require('postcss-preset-env')],
+                            }
+                        }
+                    },
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.woff2?$/i,
+                type: 'asser/resource',
+                generator: {
+                    falename: 'fonts/[name].[ext]'
+                }
+            }
+        ]
+    }
+
+
+}
+ 
