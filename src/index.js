4b6d508b9fa7c50c734975b6cb79cb9c3e46fa6a
maslo0396 <ilya97750@gmail.cm>
2025-03-20 19:26:06 +0300

    burger-menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './index.html';
 import './index.scss';
 
+const burger = document.querySelector('.burger-menu')
+const navMenu = document.querySelector('.navbar')
+
+burger.addEventListener("click",() =>{
+    burger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    burger.classList.remove("active")
+    navMenu.classList.remove("active")
+}))
